11df63f6b887104907d171fadc4c35adf8abcfec
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-25 23:54:04 +0200

    Upload very basic main

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	// getStudents "e-learning/api/student"
+)
+
+type student struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Birthdate string `json:"birthdate"`
+}
+
+var students = []student{
+	{ID: "1", Name: "Blue Train", Surname: "Coltrane", Birthdate: "2022/01/01"},
+	{ID: "2", Name: "Jeru", Surname: "Mulligan", Birthdate: "2022/01/01"},
+	{ID: "3", Name: "Sarah Vaughan and Clifford Brown", Surname: "Vaughan", Birthdate: "2022/01/01"},
+}
+
+func getStudents(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, students)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/students", getStudents)
+
+	router.Run("localhost:8080")
+}
